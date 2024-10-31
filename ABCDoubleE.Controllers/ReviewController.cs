fd8300dbc6d4009c56736eb21e1c5ce9b0341eba
Elton V <43376123+eltonvilla@users.noreply.github.com>
2024-10-30 23:02:10 -0600

    Deleted unnecessary file. Added review controller.

---
+++ ABCDoubleE.Controllers/ReviewController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using ABCDoubleE.Models;
+using ABCDoubleE.Services;
+
+namespace ABCDoubleE.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReviewController: Controller{
+     private readonly IReviewService _reviewService;
+
+    public ReviewController(IReviewService reviewService){
+        _reviewService = reviewService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> getAllReviews(){
+        
+        try{
+            return Ok(await _reviewService.GetAllReviewsAsync());
+        }
+        catch(Exception e){
+            return BadRequest("Could not get all reviews:" + e.Message);
+        }
+        
+    }
+
+    [HttpGet("GetReviewById/{reviewId}")]
+    public async Task<IActionResult> getReviewById(int reviewId){
+        
+        try{
+            return Ok(await _reviewService.GetReviewByIdAsync(reviewId));
+        }
+        catch(Exception e){
+            return BadRequest("Could not get review by id: " + e.Message);
+        }
+        
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddNewReview([FromBody] Review review){
+        
+        try{
+            await _reviewService.AddReviewAsync(review);
+            return Ok("Added review.");
+        }
+        catch(Exception e){
+            return BadRequest("Could not add review: " + e.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> EditReview([FromBody] Review review){
+        try{
+            await _reviewService.UpdateReviewAsync(review);
+            return Ok("Updated review.");
+        }
+
+        catch(Exception e){
+            return BadRequest("Could not update review: " + e.Message);
+        }
+    }
+
+    //Delete an expense
+    [HttpDelete("DeleteUserById/{reviewId}")]
+    public async Task<IActionResult> DeleteReview(int reviewId){
+        try{    
+            await _reviewService.DeleteReviewByIdAsync(reviewId);
+            return Ok("Deleted review.");
+        }
+
+        catch(Exception e){
+            return BadRequest("Could not delete review: " + e.Message);
+        }
+    }
+}

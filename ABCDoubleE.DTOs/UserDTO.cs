fea639979e60c47137a5024fe93e785f71e2d8eb
unknown <ejseliger@gmail.com>
2024-10-31 08:00:53 -0500

    Added User Services

---
+++ ABCDoubleE.DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace ABCDoubleE.DTOs;
+
+public class UserDTO {
+
+    public string userName {get; set; }= "";
+    public string password {get; set; }= "";
+    public string fullName {get; set; }= "";
+    
+}
---
+++ ABCDoubleE.Services/IUserService.cs
@@ -0,0 +1,17 @@
+using ABCDoubleE.Models;
+
+namespace ABCDoubleE.Services;
+
+public interface IUserService {
+
+    public Task<List<User>> GetAllUsers();
+
+    public Task<User> GetUserById(int id);
+
+    public Task<User> AddUser(User user);
+
+    public Task<User> UpdateUser(User user);
+
+    public Task DeleteUser(int id);
+
+}

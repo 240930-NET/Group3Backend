33e95bd205afc9286c3f407dc121b3c690f906d2
Cuong Vo <dcuongvo@gmail.com>
2024-11-01 13:32:15 -0500

    Successfully add CreateAccount

--- ABCDoubleE.API/Migrations/20241101170801_UpdateUserPasswordFields.Designer.cs
+++ ABCDoubleE.API/Migrations/20241101170801_UpdateUserPasswordFields.Designer.cs
@@ -4,6 +4,7 @@ using ABCDoubleE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ABCDoubleE.API.Migrations
 {
     [DbContext(typeof(ABCDoubleEContext))]
-    partial class ABCDoubleEContextModelSnapshot : ModelSnapshot
+    [Migration("20241101170801_UpdateUserPasswordFields")]
+    partial class UpdateUserPasswordFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ABCDoubleE.API/Migrations/20241101170801_UpdateUserPasswordFields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ABCDoubleE.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserPasswordFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "Users",
+                newName: "passwordSalt");
+
+            migrationBuilder.AddColumn<string>(
+                name: "passwordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "passwordHash",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "passwordSalt",
+                table: "Users",
+                newName: "password");
+        }
+    }
+}

094dcea0806bf06dd6bdbef93524f3129f475cc9
Elton V <43376123+eltonvilla@users.noreply.github.com>
2024-11-04 13:58:43 -0700

    Updating code

--- ABCDoubleE.API/Program.cs
+++ ABCDoubleE.API/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 using System.Text.Json;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication;
 
 
 var builder = WebApplication.CreateBuilder(args);
